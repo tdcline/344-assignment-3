7b9cf39594b60362a69ffad75d31408137bef901
Tanner Cline <tanner.cline88@gmail.com>
2021-04-28 02:54:46 -0700

    Finished variable expansion of 299887. Set up frame for built-in commands cd, status, and exit.

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 // Maximum command line length is 2048 characters
 // Need two additional for \n that getline() adds and \0 
@@ -14,6 +17,7 @@ struct userCommand {
   char* command;
   char* args[MAX_ARGS];
   int numArgs;
+  // redirects array holds up to 4 elements: the redirect symbols and their filenames
   char* redirects[4];
   int numRedirects;
   bool background;
@@ -24,8 +28,87 @@ struct userCommand {
 int displayLine(void) {
   printf(": ");
   fflush(stdout);
-  
+
   return 0;
+}
+
+/* Replaces the instance of $$ in a string with the program's pid. Takes 
+   two arguments: the pointer to the start of the user's command line and a 
+   pointer to where $$ was found in the same string. */
+char* expandVar(char* start, char* foundVar) {
+
+  // Set up variables for replacing characters in a string
+  char* newLine;
+  int startLen = strlen(start);
+  int varToEnd = strlen(foundVar);
+  int mainPid;
+  // Create a temp array to store the pid as a string in - max 10 chars goes up to 9,999,999,999
+  char temp[10];
+  // Find number of characters between start of string and instance of $$
+  int firstChars = startLen - varToEnd;
+  
+  // Get smallsh pid, store in teh temp array, and get string length
+  mainPid = getpid();
+  sprintf(temp, "%d", mainPid);
+  int pidLen = strlen(temp);
+
+  // Allocate space for new string = starting string length - 2 for the $$ plus the pid Length + 1 for the null terminator
+  newLine = calloc((startLen+pidLen-1), sizeof(char));
+
+  // Copy into newLine ptr the first part of the string
+  strncpy(newLine, start, firstChars);
+
+  // Copy into newLine ptr the pid
+  strcpy((newLine+firstChars), temp);
+
+  // Copy into newLine ptr the ending part of the string
+  strcpy((newLine+firstChars+pidLen), foundVar+2);
+
+  // Free original memory space since new pointer will replace
+  free(start);
+
+  // Return pointer for the new string
+  return newLine;
+}
+
+/* Takes a string representing the user's command, checks if the command line 
+   contains any instances of $$, and replaces those instances with the smallsh 
+   pid. Returns a pointer to the string representing the user's command line 
+   after revision if necessary. */
+char* expansionCheck(char* commandLine) {
+
+  // Set up variables for searching for $$
+  char* searchPtr = commandLine;
+  char* needle = "$$";
+  char* found;
+  char* toReturn;
+  bool changed = false;
+  bool done = false;
+
+  // Loop makes sure to search for multiple instances of $$
+  while(done != true) {
+    // Use strstr() to search for $$
+    found = strstr(searchPtr, needle);
+    // found is NULL if end of string is reached without any instances of $$
+    if(found == NULL) {
+      done = true;
+    } else {
+      // If $$ is found, replace with the smallsh pid
+      searchPtr = expandVar(searchPtr, found);
+      changed = true;
+    }
+  }
+
+  // Set return pointer to original pointer if unchanged
+  if(changed == false) {
+    toReturn = commandLine;
+  } else {
+    // Set return pointer to updated search pointer if $$ found
+    toReturn = searchPtr;
+  }
+
+  // Return the appropriate pointer for the string
+  return toReturn;
 }
 
 /* Reads the user's command and returns it as a string. A blank line will return 
@@ -36,6 +119,7 @@ char* getCommand(void) {
   char* newCommand = malloc(sizeof(MAX_COMMAND_SIZE));
   size_t len = 0;
   ssize_t numRead;
+  char* finalForm;
 
   // Referenced getline() man page
   // Read the user's command line entry into the memory pointed to with newCommand
@@ -46,9 +130,12 @@ char* getCommand(void) {
   if(strlen(newCommand) > 1) {
     newCommand[strlen(newCommand)-1] = '\0';
   }
+  
+  // Test for and expand $$ variables
+  finalForm = expansionCheck(newCommand);
 
   // Return the user's command as a string
-  return newCommand;
+  return finalForm;
 }
 
 /* Test the a command line entry for blank line or commented line. Returns true if 
@@ -65,13 +152,15 @@ bool skipTest(char* commandLine) {
     skipIt = true;
   }
 
-  // Return 
+  // Return bool result
   return skipIt;
 }
 
-/* Parse command */
+/* Takes the user's command line pointer as an argument and parses it into a 
+   userCommand struct. */
 struct userCommand *parseCommand(char* commandLine) {
   
+  // Set up variables for parsing into the command line into struct
   char* saveptr;
   int numArgs = 0;
   int numRedirects = 0;
@@ -80,10 +169,10 @@ struct userCommand *parseCommand(char* commandLine) {
   char* background = " &";
   bool redirectReached = false;
 
+  // Allocate space for new struct
   struct userCommand *newCommand = malloc(sizeof(struct userCommand));
 
-  
-  // Check for background first
+  // Check for background character first
   if(strcmp(commandLine + strlen(commandLine) - 2, background) == 0) {
     newCommand->background = true;
   } else {
@@ -117,38 +206,71 @@ struct userCommand *parseCommand(char* commandLine) {
     token = strtok_r(NULL, " ", &saveptr);
   }
 
+  // Store number of arguments and redirects for later use
   newCommand->numArgs = numArgs;
   newCommand->numRedirects = numRedirects;
 
+  // Free the token pointer and return the build struct
   free(token);
   return newCommand;
 }
+
+/* Process Command */
+int processCommand(struct userCommand *info) {
+
+  // Establish built-in commands
+  char* changeDir = "cd";
+  char* status = "status";
+  char* exit = "exit";
+
+  // Check for built-in commands first
+  if(strcmp(info->command, changeDir) == 0) {
+    // CD function
+  } else if(strcmp(info->command, status) == 0) {
+    // Status function
+  } else if(strcmp(info->command, exit) == 0) {
+    // Exit function
+  } else {
+    // Fork
+    // Exec()
+    
+  }
+
+
+
+
+  return 0;
+}
+
+
+
+
 
 
 
 /* Main function */
 int main(void) {
   
-  // allocate variables
+  // Set up variables for getting command info from user
   char* newCommand;
   struct userCommand *commandInfo;
   bool skip = false;
 
-  // display new line
+  // Display new line
   displayLine();
 
-  // get new command
+  // Get new command from user
   newCommand = getCommand();
+  // Check if blank line or comment line
   skip = skipTest(newCommand);
   if(skip != true) {
-    // parse command elements
+    // Parse command elements into struct
     commandInfo = parseCommand(newCommand);
     free(newCommand);
 
-    // testing
-    for(int i = 0; i < commandInfo->numRedirects; i++) {
-      printf("%s\n", commandInfo->redirects[i]);
-    }
+    // Process command
+    processCommand(commandInfo);
+
   }
 
   // process/do command
